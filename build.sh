6a361674f20835779e9b8aec78ad717c1aff2c8b
AnShuai666 <1028792866@qq.com>
2019-04-09 20:32:33 +0800

    更新build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,32 @@
 #!/usr/bin/env bash
+
+echo "解压词典：ORBvoc.txt.tar.gz"
+
 cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
 cd ..
+
+echo "配置并编译Thirdparty/DBoW2库..."
+cd Thirdparty/DBoW2
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
+
+echo "配置并编译Thirdparty/g2o库..."
+
+cd ../../g2o
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+cd ../../..
+
+echo "配置并编译ORB_SLAM2库..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
